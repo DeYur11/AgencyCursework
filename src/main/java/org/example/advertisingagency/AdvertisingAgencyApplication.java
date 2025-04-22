4cbbcee8a63a34dd6d69c4765ba8df483aa5c7ee
Yura <yura.debelyak@gmail.com>
2025-04-22 12:00:10 +0300

    Initial commit

---
+++ src/main/java/org/example/advertisingagency/AdvertisingAgencyApplication.java
@@ -0,0 +1,13 @@
+package org.example.advertisingagency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertisingAgencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvertisingAgencyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/advertisingagency/AdvertisingAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.advertisingagency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertisingAgencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
