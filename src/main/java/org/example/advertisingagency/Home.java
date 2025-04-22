3d2f41735d68a4558e7e3128658732d558b1158b
Yura <yura.debelyak@gmail.com>
2025-04-22 13:41:35 +0300

    Reading password from env

--- src/main/java/org/example/advertisingagency/Home.java
+++ src/main/java/org/example/advertisingagency/Home.java
@@ -1,6 +1,5 @@
 package org.example.advertisingagency;
 
-import lombok.Getter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
